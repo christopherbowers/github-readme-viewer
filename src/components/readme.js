fc6fedb17f33dd8cba20a2513cd0eddaaa066f80
Christopher Bowers <c_bowers@me.com>
2022-03-11 15:35:41 +0000

    Clean up console.logs

--- src/components/readme.js
+++ src/components/readme.js
@@ -1,21 +1,15 @@
-import { useParams } from 'react-router-dom'
 import { useState } from 'react'
 import axios from 'axios'
- // import { baseUrl } from '../baseUrl'
-
-// https:raw.githubusercontent.com/{user}/{repo}/main/README.md
 
 export default function ReadMe({user, repoName, defaultBranch}) {
 
   const [readme, setReadme] = useState(null)
-
 
   const getReadMe = async () => {
     await axios
     .get(`https://raw.githubusercontent.com/${user}/${repoName}/${defaultBranch}/README.md`)
     .then(res => {
       setReadme(res.data)
-      // console.log(res)
     })
   }
   getReadMe()
